d57e62251f2aeb045d1798cd634e8c79ae28c84d
93simon <93simonster@gmail.com>
2022-10-06 12:16:30 +0200

    json fixer + created fixed json

--- json_fixer.py
+++ json_fixer.py
@@ -11,5 +11,10 @@ for c in categories:
         print(i["clause"])
         i["clause"] = corpus_functions.normalize_fucked_encoding(i["clause"])
         print(i["clause"])
+        if "tag" in i.keys():
+            i["tag"] = i["tag"].replace("_", "")
+            for tag_num in range(2, 6):
+                if f"tag{tag_num}" in i.keys():
+                    i[f"tag{tag_num}"] = i[f"tag{tag_num}"].replace("_", "")
 
 json.dump(ds, open("balanced_dataset_fcasciola_encfix.json", "w", encoding="UTF8"), ensure_ascii=False)
