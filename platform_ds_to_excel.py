e83990e60f22521aa703178c6fc35fb3e7272ef2
93simon <93simonster@gmail.com>
2022-10-21 13:29:40 +0200

    added logic for platform library to excel

---
+++ platform_ds_to_excel.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import json
+import pandas as pd
+
+ann_folder = Path("C:/Users/smarotta/Downloads/training_senza_nowarning_e_fulldiscr/ann")
+test_folder = Path("C:/Users/smarotta/Downloads/training_senza_nowarning_e_fulldiscr/test")
+
+ann_files = list(ann_folder.glob('**/*.ann'))
+test_files = list(test_folder.glob('**/*.txt'))
+
+new_ds = []
+for a in ann_files:
+    item = {"filename": a.stem}
+    for t in test_files:
+        if a.stem == t.stem:
+            with open(t, "r", encoding="UTF8") as test:
+                print(test)
+                item["clause"] = test.read().strip()
+                print(item)
+            with open(a, "r", encoding="UTF8") as ann:
+                tags = []
+                for l in ann.readlines():
+                    # print(l.split(("\t\t"))[-1])
+                    tags.append(l.split(("\t\t"))[-1].strip())
+                print(tags)
+                item["grade"] = tags[0]
+                item["tags"] = ", ".join(tags[1:])
+            print("\n")
+    print(tags)
+    new_ds.append(item)
+print(new_ds)
+final = json.dumps(new_ds, indent=4)
+
+df = pd.DataFrame.from_records(new_ds)
+df.to_excel("new_ds.xlsx", index=False)
+print(df)
