002cfeec965d2980491f8f1e039d82cd4476dc35
93simon <93simonster@gmail.com>
2022-10-26 11:30:20 +0200

    truncate

--- companies_scraper.py
+++ companies_scraper.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 num_pages = 73
 f = open("companies_list.txt", "a", encoding="UTF8")
 f.seek(0)
+f.truncate()
 
 for i in tqdm(range(1, num_pages)):
     r = requests.get(f'https://companiesmarketcap.com/page/{i}/')
@@ -14,6 +15,6 @@ for i in tqdm(range(1, num_pages)):
     rows = table.findAll(lambda tag: tag.name=='tr')
     for r in rows[1:]:
         name = [i.text.strip() for i in r.find_all("div", class_="company-name")][0]
-        f.write(name+"\n")
+        f.write(name.lower()+"\n")
 
 f.close()
